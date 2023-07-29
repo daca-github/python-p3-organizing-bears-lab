eedf9cf8713c5288f0395356a863c16f6df651cd
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-07-29 18:11:06 -0400

    .

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
-('Mr. Chocolate', 5, 'M', 'Brown', 'Gentle', TRUE),
-('Rowdy', 3, 'M', 'Black', 'Playful', TRUE),
-('Tabitha', 7, 'F', 'Gray', 'Calm', TRUE),
-('Sergeant Brown', 4, 'M', 'Brown', 'Aggressive', TRUE),
-('Melissa', 6, 'F', 'White', 'Shy', TRUE),
-('Grinch', 2, 'M', 'Green', 'Mischievous', TRUE),
-('Wendy', 4, 'F', 'Brown', 'Friendly', TRUE),
-(NULL, 8, 'M', 'Black', 'Active', TRUE);
+('Mr. Chocolate', 5, 'M', 'Brown', 'Gentle', 1),
+('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+('Tabitha', 7, 'F', 'Gray', 'Calm', 1),
+('Sergeant Brown', 4, 'M', 'Brown', 'Aggressive', !),
+('Melissa', 6, 'F', 'White', 'Shy', 1),
+('Grinch', 2, 'M', 'Green', 'Mischievous', 1),
+('Wendy', 4, 'F', 'Brown', 'Friendly', 1),
+(NULL, 8, 'M', 'Black', 'Active', 1);
